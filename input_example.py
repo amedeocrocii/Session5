ab4be79c0c6b61e3b74b0f43cc56f5c61a12ea19
amedeocroci <amedeo.croci@gmail.com>
2025-01-22 17:18:47 +0100

    input example

--- input_example.py
+++ input_example.py
@@ -1,4 +1,16 @@
 name=input("what is your name?")
 print("hello", name)
 age= input("How old are you?")
-print("you were probably born in", 2024-int(age))
+try:
+    age=int(age) # i am trying to convert it to a number
+    print("you were probably born in", 2024-int(age))
+    new_age = age/0
+except ValueError:
+    print("you are trying to trick me")
+    print("better luck next time")
+except ZeroDivisionError:
+    print("you can't divide by zero")
+except:
+    print("something unexpected happened")
+else:
+    print("you were probably born in", 2024 - age)
